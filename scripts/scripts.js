761c05bcd8765206c807dbb27d9969848b835c0c
Bruno Henrique <124411061+BrunoHenrique12@users.noreply.github.com>
2023-06-07 08:40:07 -0300

    temperatura minima e maxima adicionada

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,10 +2,15 @@ const key = "ad6700132703ce7572ca15ca34a07b19"
 
 function colocarDadosNaTela(dados){
     document.querySelector(".cidade").innerHTML = dados.name
-    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".temp").innerHTML = "Agora " + Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".minima").innerHTML = "Mínima: " + Math.floor(dados.main.temp_min) + "°C"
+    document.querySelector(".maxima").innerHTML = " | Maxima: " + Math.floor(dados.main.temp_max) + "°C"
     document.querySelector(".text-previsao").innerHTML = dados.weather[0].description
     document.querySelector(".previsao").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
     document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%"
+    document.querySelector(".wind").innerHTML = " | Vento: " + Math.floor(dados.wind.speed) + " Km/h "
+    
+    
 }
 
 async function buscarCidade(cidade) {
